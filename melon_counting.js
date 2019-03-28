0e22d395237affd56367b76435cf5d93dad2064d
Melissa <melissascampini@gmail.com>
2019-03-27 19:23:25 -0700

    completed

---
+++ melon_counting.js
@@ -0,0 +1,28 @@
+
+const melonsList = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+function countMelons(melonsArray) {
+
+        const melons = {};
+
+        for (let melon of melonsArray) {
+
+            if(melons[melon]) {
+                melons[melon] += 1
+            } else {
+                melons[melon] = 1
+            }
+        }
+        return melons
+}
+console.log(countMelons(melonsList));
